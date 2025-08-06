79703a61acac3ff4c46dfe7c19f1508186915fcf
Wimmer <copilotshub@gmail.com>
2025-08-06 10:30:24 +0800

    feat(transfer): 1、转账交易只允许本机访问

--- api/v1/routes.py
+++ api/v1/routes.py
@@ -5,7 +5,7 @@ from services.transaction_service import transfer
 from database import SessionLocal
 router = APIRouter()
 
-
+block_ip_list = []
 
 # 创建依赖项
 def get_db():
@@ -18,6 +18,9 @@ def get_db():
 @router.get("/transactions/{transaction_id}")
 def transfer_transaction(transaction_id: int,request:Request,db: Session = Depends(get_db)):
     client_ip = request.client.host
+    if client_ip in block_ip_list:
+        raise HTTPException(status_code=403, detail="Forbidden: Block IP")
     if client_ip not in ("127.0.0.1", "localhost", "::1"):
+        block_ip_list.append(client_ip)
         raise HTTPException(status_code=403, detail="Forbidden: Only localhost allowed")
     return transfer(transaction_id,db)
