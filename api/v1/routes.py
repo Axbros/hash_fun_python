6ea1c4ab196dc8a95e5b2053ec68a289706d9357
Wimmer <copilotshub@gmail.com>
2025-08-06 15:08:30 +0800

    feat(transfer): 1、转账交易只允许本机访问

--- api/v1/routes.py
+++ api/v1/routes.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends, HTTPException,Request
 from sqlalchemy.orm import Session
-
+import logging
 from services.transaction_service import transfer
 from database import SessionLocal
 router = APIRouter()
@@ -19,8 +19,10 @@ def get_db():
 def transfer_transaction(transaction_id: int,request:Request,db: Session = Depends(get_db)):
     client_ip = request.client.host
     if client_ip in block_ip_list:
+        logging.warn("Block IP Request,IP address:"+client_ip)
         raise HTTPException(status_code=403, detail="Forbidden: Block IP")
     if client_ip not in ("127.0.0.1", "localhost", "::1"):
         block_ip_list.append(client_ip)
+        logging.warn("Not localhost request,IP address:"+client_ip)
         raise HTTPException(status_code=403, detail="Forbidden: Only localhost allowed")
     return transfer(transaction_id,db)
--- queen/worker.py
+++ queen/worker.py
@@ -14,16 +14,20 @@ def transaction_worker(task_queue: Queue):
 
         try:
             tx_info = task.client.get_transaction_info(task.tx_id)
+            logging.info(f"tx_info={tx_info}")
             payload = task.payload_builder(tx_info)
 
             result = tx_info.get("receipt", {}).get("result")
-            logging.info(f"🔍 链上状态 result={result}")
-
-            if result != "SUCCESS":
-                logging.info(result)
-                logging.warning(f"⚠️ 链上交易失败：{task.tx_id}")
+            if result is None:
+                # 没有 result 字段 → 非合约调用，比如 TRX 转账
+                logging.info("ℹ️ 非合约交易（无 result 字段），假设已成功")
             else:
-                logging.info(f"✅ 链上交易成功，写入数据库")
+                logging.info(f"🔍 链上状态 result={result}")
+                if result != "SUCCESS":
+                    logging.info(result)
+                    logging.warning(f"⚠️ 链上交易失败：{task.tx_id}")
+                else:
+                    logging.info(f"✅ 链上交易成功，写入数据库")
             insert_transfer_record(task.db_session, payload, task.transaction_id)
         except Exception as e:
             logging.error(f"🔥 异常：{e}")
